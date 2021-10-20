596634c1d3b276ac20c0240e49b924158b272bcf
Vivek Ragunathan <vivekragunathan@gmail.com>
2021-10-20 03:01:33 -0700

    Manually rebuilding site

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-cd public
+# cd public
 
 # Add changes to git.
 git add .
@@ -19,6 +19,7 @@ msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
+
 git commit -m "$msg"
 
 # Push source and build repos.
